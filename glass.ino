0b223aafcd0641979b0314bd426bfe8ad8217b83
cr0issant <matthieu.pradeau@aquilenet.fr>
2017-04-18 22:29:09 +0200

    Stop mode auto mais pas top
    seule solution un delay(100); et c'est nul

--- glass.ino
+++ glass.ino
@@ -220,31 +220,37 @@ void loop()
     // ------------------- Partie 1/4 ------------------- 
     digitalWrite(Electrovanne1, LOW); // Electrovanne 1 Fermé Led verte Pression
     digitalWrite(Electrovanne2, HIGH); // Electrovanne 2 Ouverte Led bleu Depression
-    MiseEnPression ( Pression_1, Cycle_1, 7, CapteurPression1, 0, Cycle_1, atm, Phase ); // -800
+    message = MiseEnPression ( Pression_1, Cycle_1, 7, CapteurPression1, 0, Cycle_1, atm, Phase ); // -800
 
     // ------------------- Partie 2/4 ------------------- 
     digitalWrite(Electrovanne1, HIGH); // Electrovanne 1 Ouverte Led verte Pression
     digitalWrite(Electrovanne2, LOW); // Electrovanne 2 Fermé Led bleu Depression
-    MiseEnPression ( Pression_2, Cycle_2, 105, CapteurPression1, Cycle_1, Cycle_1 + Cycle_2, atm, Phase); // 2500
+    if ( message != Arret ) { message = MiseEnPression ( Pression_2, Cycle_2, 105, CapteurPression1, Cycle_1, Cycle_1 + Cycle_2, atm, Phase); } // 2500
 
     // ------------------- Partie 3/4 ------------------- 
     digitalWrite(Electrovanne1, LOW); // Electrovanne 1 Fermé Led verte Pression
     digitalWrite(Electrovanne2, HIGH); // Electrovanne 2 Ouverte Led bleu Depression
-    MiseEnPression ( Pression_3, Cycle_3, 7, CapteurPression1, Cycle_1 + Cycle_2, Cycle_1 + Cycle_2 + Cycle_3, atm, Phase ); // -800
+    if ( message != Arret ) { message = MiseEnPression ( Pression_3, Cycle_3, 7, CapteurPression1, Cycle_1 + Cycle_2, Cycle_1 + Cycle_2 + Cycle_3, atm, Phase ); } // -800
+
     
     // ------------------- Partie 4/4 ------------------- 
     digitalWrite(Electrovanne1, HIGH); // Electrovanne 1 Ouverte Led verte Pression
     digitalWrite(Electrovanne2, LOW); // Electrovanne 2 Fermé Led bleu Depression
-    MiseEnPression ( Pression_4, Cycle_4, 120, CapteurPression1, Cycle_1 + Cycle_2 + Cycle_3, CycleTotal, atm, Phase ); // 3000
+    if ( message != Arret ) { message = MiseEnPression ( Pression_4, Cycle_4, 120, CapteurPression1, Cycle_1 + Cycle_2 + Cycle_3, CycleTotal, atm, Phase ); } // 3000
 
+    digitalWrite(Electrovanne1, LOW); // Electrovanne 1 Led verte Pression
+    digitalWrite(Electrovanne2, LOW); // Electrovanne 2 Led bleu Depression
+    digitalWrite(Pompe, LOW);
     EventChoix = false;
   }
   else { }
 }
 
 
-void MiseEnPression ( int Pression, int Cycle, int ForcerPression, int CapteurPression1, int Etape, int EtapeSuivante, float atm, bool Phase )
+String MiseEnPression ( int Pression, int Cycle, int ForcerPression, int CapteurPression1, int Etape, int EtapeSuivante, float atm, bool Phase )
 { 
+    String message = nex.listen();
+    String Arret = "65 2 7 1 ffff ffff ffff"; // Page 2  - Arrêt réparation "b2"
     nextionSerial.print("z0.val=");
     nextionSerial.print(map(CheckPression ( CapteurPression1, atm, Phase ), -1000, 3000, 0, 120));
     nextionSerial.write(0xff);
@@ -254,9 +260,17 @@ void MiseEnPression ( int Pression, int Cycle, int ForcerPression, int CapteurPr
     
     // Tant que la pression n'est pas atteinte
     bool VerifPression = 0;
-    while ( VerifPression != 1 )
+    while ( ( VerifPression != 1 ) && ( message != Arret ) )
     {
+      message = nex.listen();
+      if ( message == Arret ) 
+      { 
+        message = Arret; VerifPression = 1; return Arret; 
+      }
+      else 
+      {
       VerifPression = EtatDeLaPression ( Pression, CheckPression ( CapteurPression1, atm, Phase ) );
+      //*
       nextionSerial.print("z0.val=");
       nextionSerial.print(map( CheckPression ( CapteurPression1, atm, Phase ), -1000, 3000, 0, 120 ) );
       nextionSerial.write(0xff);
@@ -268,14 +282,18 @@ void MiseEnPression ( int Pression, int Cycle, int ForcerPression, int CapteurPr
       nextionSerial.write(0xff);
       nextionSerial.write(0xff);
       nextionSerial.write(0xff);
-      delay(1);
+      //*/
+      }
+      delay(100);
+
     }
 
     // Effet visuel de la progression sur XX secondes
     unsigned long currentMillis = millis();
     unsigned long previousMillis = millis();
-    while ( (currentMillis - previousMillis) < (Cycle * 1000) )
+    while ( ( (currentMillis - previousMillis) < (Cycle * 1000) ) && ( message != Arret ) )
     {
+
         // Ajouter condition pour envoyer la pression en cas de manque
         currentMillis = millis();
         AvancementEtape = map(currentMillis - previousMillis, 0, Cycle * 1000, 0, 100);
@@ -296,6 +314,8 @@ void MiseEnPression ( int Pression, int Cycle, int ForcerPression, int CapteurPr
         nextionSerial.write(0xff);
         delay(1);
         EtatDeLaPression ( Pression, CheckPression ( CapteurPression1, atm, Phase ) );
+        message = nex.listen();
+        if ( message == Arret ) { message = Arret; return Arret; }
     }
     digitalWrite(Electrovanne1, LOW); // Electrovanne 1 Led verte Pression
     digitalWrite(Electrovanne2, LOW); // Electrovanne 2 Led bleu Depression
